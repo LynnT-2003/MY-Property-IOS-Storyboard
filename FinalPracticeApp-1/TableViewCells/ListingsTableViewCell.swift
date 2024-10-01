ec502fd98a457fc94517563609da62cba0871771
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-10-02 05:37:54 +0700

    Updated UI.

--- FinalPracticeApp-1/TableViewCells/ListingsTableViewCell.swift
+++ FinalPracticeApp-1/TableViewCells/ListingsTableViewCell.swift
@@ -14,9 +14,35 @@ class ListingsTableViewCell: UITableViewCell {
         didSet {
             // Update the listingName label when the listing property is set
             listingName.text = listing?.listingName
-            listingFurniture.text = "Room: \(listing?.furniture ?? "")"
-            listingPrice.text = "\(listing?.price ?? 0) THB/mo"
-            listingStatus.text = "Status: \(listing?.statusActive ?? "not active")"
+//            listingFurniture.text = "Room: \(listing?.furniture ?? "")"
+//            listingPrice.text = "\(listing?.price ?? 0) THB / mo"
+//            listingStatus.text = "Status: \(listing?.statusActive ?? "not active")"
+            
+            if let listing = listing {
+                // Style for furniture
+                let furnitureText = "Room: \(listing.furniture)"
+                let attributedFurnitureText = NSMutableAttributedString(string: furnitureText)
+                attributedFurnitureText.addAttribute(.foregroundColor, value: UIColor(red: 0, green: 0.5, blue: 0, alpha: 1), range: NSRange(location: 6, length: listing.furniture.count)) // Dark green
+                listingFurniture.attributedText = attributedFurnitureText
+
+                // Style for price
+                let priceText = "\(Int(listing.price.rounded())) THB / month"
+                let attributedPriceText = NSMutableAttributedString(string: priceText)
+                
+                // Set the entire text color to default
+                attributedPriceText.addAttribute(.foregroundColor, value: UIColor.black, range: NSRange(location: 0, length: attributedPriceText.length))
+                
+                // Change the color for only the amount part
+                attributedPriceText.addAttribute(.foregroundColor, value: UIColor.blue, range: NSRange(location: 0, length: "\(Int(listing.price.rounded()))".count + 4) )
+                listingPrice.attributedText = attributedPriceText
+
+                // Style for status
+                let statusText = "Status: \(listing.statusActive)"
+                let attributedStatusText = NSMutableAttributedString(string: statusText)
+                attributedStatusText.addAttribute(.foregroundColor, value: UIColor(red: 0, green: 0.5, blue: 0, alpha: 1), range: NSRange(location: 8, length: (listing.statusActive.count))) // Dark green
+                listingStatus.attributedText = attributedStatusText
+            }
+
 
             // Load the image using the buildImageURL function
             
