f72f5103a2591771059b40304245be3563856ec0
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-10-01 23:29:42 +0700

    Latest update with routes and walking distance.

--- FinalPracticeApp-1/ViewControllers/ListingDetailsViewController.swift
+++ FinalPracticeApp-1/ViewControllers/ListingDetailsViewController.swift
@@ -67,7 +67,17 @@ class ListingDetailsViewController: UIViewController {
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
         annotation.title = property?.title
-        annotation.subtitle = listing?.listingName
+//        annotation.subtitle = listing
+        
+        if let propertyLat = property?.latitude, let propertyLng = property?.longitude {
+            let propertyCoordinate = CLLocationCoordinate2D(latitude: propertyLat, longitude: propertyLng)
+            let destinationCoordinate = CLLocationCoordinate2D(latitude: destinationLat, longitude: destinationLng)
+            
+            calculateWalkingDistanceAndDrawRoute(from: propertyCoordinate, to: destinationCoordinate) { distanceInKilometers in
+                // Show an alert with the property description and walking distance
+                annotation.subtitle = "\(String(format: "%.2f", distanceInKilometers)) km from ABAC"
+            }
+        }
         
         // Add the marker to the map
         listingMapView.addAnnotation(annotation)
@@ -80,7 +90,6 @@ class ListingDetailsViewController: UIViewController {
 }
 
 extension ListingDetailsViewController: MKMapViewDelegate {
-    
     
     // Delegate method to customize the annotation view
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -109,14 +118,14 @@ extension ListingDetailsViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         if control == view.rightCalloutAccessoryView {
             
-            // Calculate walking distance
+            // Calculate walking distance and draw route
             if let propertyLat = property?.latitude, let propertyLng = property?.longitude {
                 let propertyCoordinate = CLLocationCoordinate2D(latitude: propertyLat, longitude: propertyLng)
                 let destinationCoordinate = CLLocationCoordinate2D(latitude: destinationLat, longitude: destinationLng)
                 
-                calculateWalkingDistance(from: propertyCoordinate, to: destinationCoordinate) { distanceInKilometers in
+                calculateWalkingDistanceAndDrawRoute(from: propertyCoordinate, to: destinationCoordinate) { distanceInKilometers in
                     // Show an alert with the property description and walking distance
-                    let alertMessage = "Walking distance from Assumption University: \(String(format: "%.2f", distanceInKilometers)) km"
+                    let alertMessage = "Walking distance: \(String(format: "%.2f", distanceInKilometers)) km"
                     let alert = UIAlertController(title: "Property Information", message: alertMessage, preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                     self.present(alert, animated: true, completion: nil)
@@ -125,8 +134,8 @@ extension ListingDetailsViewController: MKMapViewDelegate {
         }
     }
     
-    // Function to calculate walking distance and execute a completion handler with the result
-    func calculateWalkingDistance(from originCoordinate: CLLocationCoordinate2D, to destinationCoordinate: CLLocationCoordinate2D, completion: @escaping (Double) -> Void) {
+    // Function to calculate walking distance, draw route, and execute a completion handler with the result
+    func calculateWalkingDistanceAndDrawRoute(from originCoordinate: CLLocationCoordinate2D, to destinationCoordinate: CLLocationCoordinate2D, completion: @escaping (Double) -> Void) {
         let sourcePlacemark = MKPlacemark(coordinate: originCoordinate)
         let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
         
@@ -142,14 +151,33 @@ extension ListingDetailsViewController: MKMapViewDelegate {
                 completion(0) // Return 0 in case of an error
                 return
             }
-            
+
             if let route = response?.routes.first {
                 let distanceInMeters = route.distance
                 let distanceInKilometers = distanceInMeters / 1000.0
+                
+                // Draw the route on the map
+                self.listingMapView.addOverlay(route.polyline)
+               
+    
+                // Adjust the map region to fit the route
+                self.listingMapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50), animated: true)
+                
                 completion(distanceInKilometers)
             } else {
                 completion(0) // Return 0 if no route is found
             }
         }
+    }
+    
+    // Render the route on the map
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let renderer = MKPolylineRenderer(polyline: polyline)
+            renderer.strokeColor = .blue
+            renderer.lineWidth = 3.0
+            return renderer
+        }
+        return MKOverlayRenderer()
     }
 }
